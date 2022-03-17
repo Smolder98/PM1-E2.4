f882cf7f4f0bbddd09be4d7c07c1696190a6d527
Smolder98 <alicoescobar@gmail.com>
2022-03-16 21:01:40 -0600

    primer commit

---
+++ app/src/main/java/com/aplicacion/pm1_e24/MainActivity.java
@@ -0,0 +1,14 @@
+package com.aplicacion.pm1_e24;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
